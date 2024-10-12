7f8260d69782642bb3134c0c0467077ddf286444
Peter Huang <huangcx22@mails.tsinghua.edu.cn>
2024-10-12 19:38:03 +0800

    move language to objects, add weak normalization

--- src/main.js
+++ src/main.js
@@ -1,134 +1,199 @@
-const Maker = {
-  // lambda, application, constant
-  l: (fn) => (S) => (mustHave(S, "l"), S.l((x) => fn((_) => x)(S))), // tricky
-  a: (e1, e2) => (S) => (mustHave(S, "a"), S.a(e1(S), e2(S))),
-  c: (v) => (S) => (mustHave(S, "c"), S.c(v)),
-
-  // type arrow, type constant
-  to: (t1, t2) => (S) => (mustHave(S, "to"), S.to(t1(S), t2(S))),
-  t: (n) => (S) => (mustHave(S, "t"), S.t(n)),
-
-  // kind *
-  star: (S) => (mustHave(S, "star"), S.star),
-
-  // type annotation
-  of: (e, t) => (S) => (mustHave(S, "of"), S.of(e(S), t(S))),
-};
-
-const EvalS = {
-  l: (fn) => fn,
-  a: (e1, e2) => e1(e2),
-  c: (v) => v,
-  of: (e, _) => e,
-
-  to: (t1, t2) => undefined,
-  t: (_) => undefined,
-  star: undefined,
-};
-
-function eva(term) {
-  return term(EvalS);
+class ADT {
+  static sym = Symbol("type");
+  constructor(props, options = { show: false }) {
+    this.show = options.show;
+    // todo: move visitor to prototype
+    for (const k of Object.keys(props)) {
+      const len = props[k];
+      this[k] = function (...args) {
+        if (args.length != len)
+          throw Error(
+            `Constructor ${k} should receive ${len} arguments but got ${args.length}`
+          );
+        const visitor = {
+          match(branches) {
+            if (!Object.hasOwn(branches, k))
+              throw Error(`Invalid match: did not match ${k}`);
+            return branches[k](...args);
+          },
+        };
+        if (this.show) {
+          visitor.type = k;
+          visitor.params = args;
+        }
+        visitor[ADT.sym] = this;
+        Object.freeze(visitor);
+        return visitor;
+      }.bind(this);
+    }
+  }
+  hasInstance(obj) {
+    return Object.hasOwn(obj, ADT.sym) && Object.hasOwn(obj[ADT.sym], this);
+  }
 }
 
-// term == Int -> String
-const ShowS = {
-  l: (fn) => (level) => {
-    // fn :: term a -> term b == (Int -> String) -> (Int -> String)
-    const x = `x${level}`;
-    return `(λ${x}. ${fn((_) => x)(level + 1)})`;
-  },
-  a: (e1, e2) => (level) => `(${e1(level)} ${e2(level)})`,
-  c: (v) => (_) => `${v}`,
+// a language has: eval, norm, show
+// this may not be the best way to represent a language:
+// it forfeits tagless final's composability?
 
-  to: (t1, t2) => level => `(${t1(level)} -> ${t2(level)})`,
-  t: n => _ => `${n}`,
-  star: _ => "*",
+const Main = (() => {
+  const Lang = {
+    // lambda, application, constant
+    l: (fn) => (S) => (mustHave(S, "l"), S.l((x) => fn((_) => x)(S))), // tricky
+    a: (e1, e2) => (S) => (mustHave(S, "a"), S.a(e1(S), e2(S))),
+    c: (v) => (S) => (mustHave(S, "c"), S.c(v)),
 
-  of: (e, t) => (level) => `${e(level)} :: ${t(level)}`,
-};
+    // type arrow, type constant
+    to: (t1, t2) => (S) => (mustHave(S, "to"), S.to(t1(S), t2(S))),
+    t: (n) => (S) => (mustHave(S, "t"), S.t(n)),
 
-function show(term) {
-  return term(ShowS)(0);
-}
+    // kind *
+    star: (S) => (mustHave(S, "star"), S.star),
+
+    // type annotation
+    of: (e, t) => (S) => (mustHave(S, "of"), S.of(e(S), t(S))),
+  };
+
+  // Evaluation by translating to js functions
+  // term a == a
+  Lang.EvalS = {
+    l: (fn) => fn,
+    a: (e1, e2) => e1(e2),
+    c: (v) => v,
+  };
+  Lang.eval = function (term) {
+    return term(this.EvalS);
+  }
+
+  // Translate term to string
+
+  // term a == Int -> String
+  Lang.ShowS = {
+    l: (fn) => (level) => {
+      // fn :: term a -> term b == (Int -> String) -> (Int -> String)
+      const x = `x${level}`;
+      return `(λ${x}. ${fn((_) => x)(level + 1)})`;
+    },
+    a: (e1, e2) => (level) => `(${e1(level)} ${e2(level)})`,
+    c: (v) => (_) => `${v}`,
+
+    to: (t1, t2) => (level) => `(${t1(level)} -> ${t2(level)})`,
+    t: (n) => (_) => `${n}`,
+    star: (_) => "*",
+
+    of: (e, t) => (level) => `${e(level)} :: ${t(level)}`,
+  };
+
+  Lang.show = function (term) {
+    return term(this.ShowS)(0);
+  }
+
+  return Lang;
+})();
+
+// Translate term to string
+// Normalization
+// we extend evaluation's return value to include terms.
+// V = I Int | T Term | A (V -> V)
+
+const Full = (() => {
+  const Lang = {
+    l: (t) => (S) => (mustHave(S, "l"), S.l(t(S))),
+    a: (t1, t2) => (S) => (mustHave(S, "a"), S.a(t1(S), t2(S))),
+    v: (n) => (S) => (mustHave(S, "v"), S.v(n)),
+    c: (v) => (S) => (mustHave(S, "c"), S.c(v)),
+  };
+  const Value = new ADT({ C: 1, A: 1, T: 1 });
+
+  Lang.EvalS = {
+    l: (t) => (env) => (x) => t([x, ...env]),
+    a: (t1, t2) => (env) => t1(env)(t2(env)),
+    v: (n) => (env) => env[n],
+    c: (v) => (_) => v,
+  };
+  Lang.eval = function (term) {
+    return term(this.EvalS);
+  }
+
+  function readBack(value) {
+    return value.match({
+      C: (n) => Lang.c(n),
+      T: (e) => e,
+      A: (f) => Lang.l(readBack(f(Value.T(Lang.v(0))))),
+    });
+  }
+
+  // normalize a term. does not reach under lambdas!
+  // term a == env -> V a
+  // env = [V a]
+  Lang.NormS = {
+    l: (t) => (env) => {
+      const otherwise = () => Value.A((x) => t([x, ...env])); // regular evaluation
+      if (env.length == 0) return otherwise();
+      return env[0].match({
+        T: () => Value.T(Lang.l(readBack(t([Value.T(Lang.v(0)), ...env])))), // readback
+        A: otherwise,
+        C: otherwise,
+      });
+    },
+    a: (t1, t2) => (env) => {
+      const otherwise = () =>
+        t1(env).match({
+          A: (f) => f(t2(env)),
+        });
+      if (env.length == 0) return otherwise();
+      return env[0].match({
+        T: () => Value.T(Lang.a(readBack(t1(env)), readBack(t2(env)))), // readback
+        A: otherwise,
+        C: otherwise,
+      });
+    },
+    v: (n) => (env) =>
+      env[n].match({
+        T: () => Value.T(Lang.v(n)), // readback
+        A: () => env[n],
+        C: () => env[n],
+      }),
+    c: (v) => (env) => {
+      const otherwise = () => Value.C(v);
+      if (env.length == 0) return otherwise();
+      return env[0].match({
+        T: () => Lang.c(v), // readback
+        A: otherwise,
+        C: otherwise,
+      });
+    },
+  };
+  Lang.norm = function (term) {
+    const value = term(this.NormS)([]);
+    return readBack(value);
+  }
+
+  // strong normalization?
+
+  // term a == level -> a
+  Lang.ShowS = {
+    l: (t) => (level) => {
+      const x = `x${level}`;
+      return `(λ${x}. ${t(level + 1)})`;
+    },
+    a: (t1, t2) => (level) => `(${t1(level)} ${t2(level)})`,
+    v: (n) => (level) => `x${level - n - 1}`,
+    c: (v) => (_) => `${v}`,
+  };
+  Lang.show = function (term) {
+    return term(this.ShowS)(0);
+  }
+
+  return Lang;
+})();
+
+// Into ADT
 
 const MatchS = {
   to: (a0, b0) => (S) => (mustHave(S, "to"), S.to(a0, b0)),
   t: (n0) => (S) => (mustHave(S, "t"), S.t(n0)),
 };
-
-// term == term -> bool
-const EqS = {
-  to: (a0, b0) => (t) =>
-    t(MatchS)({
-      to: (a1, b1) => a0(a1) && b0(b1),
-      t: (_) => false,
-    }),
-  t: (n0) => (t) =>
-    t(MatchS)({
-      t: (n1) => n0 === n1,
-      to: (_0, _1) => false,
-    }),
-};
-
-function eq(a, b) {
-  return a(EqS)(b);
-}
-
-// term == [Result type, type -> Result ()]
-const KindCheckS = {
-  to: ([a, _ca], [b, _cb]) => {
-    const term = Maker.to(a, b);
-    return [term, assertTypeEq(term)];
-  },
-  t: (n) => {
-    const term = Maker.t(n);
-    return [term, assertTypeEq(term)];
-  },
-};
-
-// term == [Result type, type -> Result ()]
-// an inferer with a checker
-const TypeInferS = {
-  of: ([_t, c], [tAnno, _cAnno]) => {
-    c(tAnno);
-    return [tAnno, assertTypeEq(tAnno)];
-  },
-  a: ([t1, _c1], [_t2, c2]) =>
-    t1(MatchS)({
-      to: (a, b) => {
-        c2(a);
-        return [b, assertTypeEq(b)];
-      },
-    }),
-  c: (n) => {
-    let type = undefined;
-    if(Number.isInteger(n)) type = "Int";
-    if(typeof n === "string") type = "String";
-    if(typeof n === "boolean") type = "Bool";
-    const term = Maker.t(n);
-    return [term, assertTypeEq(term)];
-  },
-};
-
-const TypeCheckS = {
-  // fn :: ([Result type, type -> Result ()]) -> [Result type, type -> Result ()], t :: type
-  l: (fn) => [undefined, (t) => {
-    const [lhs, rhs] = t(MatchS)({
-      to: (a, b) => [a, b],
-    });
-    fn([rhs, assertTypeEq(rhs)])[1](lhs);
-  }],
-};
-
-const TypingS = {
-  ...KindCheckS,
-  ...TypeInferS,
-  ...TypeCheckS,
-}
-
-function typeCheck(term) {
-  return term(TypingS);
-}
 
 // Appendix
 
@@ -139,13 +204,6 @@ function mustHave(obj, key) {
 
 function merge(...objs) {
   return objs.reduce((acc, obj) => Object.assign(acc, obj), {});
-}
-
-function assertTypeEq(expected) {
-  return (got) => {
-    if (!eq(expected, got))
-      throw Error(`Expected type ${show(expected)} but inferred ${show(got)}`);
-  };
 }
 
 function dbg(...args) {
--- src/test.js
+++ src/test.js
@@ -1,32 +1,70 @@
-const tests = [
-  () => {
-    const { l, a, c } = Makers1;
-    const k = l((x) => l((y) => x));
-    const i = l((x) => x);
-    const ki = a(k, i);
-    const a2 = (x, y, z) => a(a(x, y), z);
-    return [a2(k, c(1), c(2)), a2(ki, c(1), c(2))].flatMap((t) => [
-      ev1(t),
-      show1(t),
-    ]);
-  },
-];
+const { l, a, c, v } = Full;
 
-const displayEl = document.getElementById("display");
+const k = l(l(v(1)));
+const i = l(v(0));
+const ki = a(k, i);
+console.log(Full.show(ki));
+console.log(Full.show(Full.norm(ki)));
 
-// tests.forEach((test, i) => {
-//   const [ev, show] = test();
-//   const evEl = document.createElement("div");
-//   evEl.innerHTML = `Test ${i + 1}: ${ev}`;
-//   displayEl.appendChild(evEl);
-// });
-
-function h(tag, attrs, children) {
-  const el = document.createElement(tag);
-  Object.entries(attrs).forEach(([k, v]) => el.setAttribute(k, v));
-  const childEls = children.map((c) =>
-    typeof c === "string" ? document.createTextNode(c) : c
-  );
-  childEls.forEach((c) => el.appendChild(c));
-  return el;
+function toChurch(n) {
+  if(n === 0) return l(l(v(0)));
+  const f = v(1);
+  const x = v(0);
+  return l(l(a(a(toChurch(n - 1), f), a(f, x))));
 }
+function addChurch(m, n) {
+  const f = v(1);
+  const x = v(0);
+  return l(l(a(a(m, f), a(a(n, f), x))));
+}
+const two = toChurch(2);
+const three = toChurch(3);
+const fiveAdd = addChurch(two, three);
+const five = toChurch(5);
+console.log(Full.show(fiveAdd));
+console.log(Full.show(Full.norm(fiveAdd)));
+console.log(Full.show(five));
+
+// const { l, a, c } = Maker;
+// function toChurch(n) {
+//   return n === 0
+//     ? l((f) => l((x) => x))
+//     : l((f) => l((x) => a(a(toChurch(n - 1), f), a(f, x))));
+// }
+// const twice = toChurch(2);
+// const thrice = toChurch(3);
+// logshow(twice);
+// logshow(normalize(twice));
+
+// const tests = [
+//   () => {
+//     const { l, a, c } = Makers1;
+//     const k = l((x) => l((y) => x));
+//     const i = l((x) => x);
+//     const ki = a(k, i);
+//     const a2 = (x, y, z) => a(a(x, y), z);
+//     return [a2(k, c(1), c(2)), a2(ki, c(1), c(2))].flatMap((t) => [
+//       ev1(t),
+//       show1(t),
+//     ]);
+//   },
+// ];
+
+// const displayEl = document.getElementById("display");
+
+// // tests.forEach((test, i) => {
+// //   const [ev, show] = test();
+// //   const evEl = document.createElement("div");
+// //   evEl.innerHTML = `Test ${i + 1}: ${ev}`;
+// //   displayEl.appendChild(evEl);
+// // });
+
+// function h(tag, attrs, children) {
+//   const el = document.createElement(tag);
+//   Object.entries(attrs).forEach(([k, v]) => el.setAttribute(k, v));
+//   const childEls = children.map((c) =>
+//     typeof c === "string" ? document.createTextNode(c) : c
+//   );
+//   childEls.forEach((c) => el.appendChild(c));
+//   return el;
+// }
